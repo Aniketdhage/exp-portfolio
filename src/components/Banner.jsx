881611f1cd7e421ec61b862516d3f47598c70dcd
Aniketdhage <aniketdhage90@gmail.com>
2023-01-20 08:37:25 +0530

    update: added skills section with the help of react multi carousual

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 function Banner() {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(0.1 - Math.random() * 100);
+  const toRotate = [
+    "Web Developer",
+    "Front End Developer",
+    "ReactJS Developer",
+  ];
+  const period = 200;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let UpdatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(UpdatedText);
+
+    if (isDeleting) {
+      setDelta((prev) => prev / 2);
+    }
+
+    if (!isDeleting && UpdatedText == fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && UpdatedText == "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -11,7 +53,7 @@ function Banner() {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm webdecoded`} <span className="wrap">Web Developer</span>
+              {`Hi I'm `} <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita
